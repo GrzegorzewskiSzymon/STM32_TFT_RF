53089b51f7afaab3dc9b4596119d0b9309f8c6cd
GrzegorzewskiSzymon <szymon.s.grzegorzewski@gmail.com>
2022-12-29 16:28:55 +0100

    add: define ILI9341 registers addresses

---
+++ TFT_ILI9341.c
@@ -0,0 +1,17 @@
+/*
+ * TFT_ILI9341.c
+ *
+ *  Created on: 29 Dec 2022
+ *      Author: Szymon Grzegorzewski
+ */
+
+#include <stdint.h>
+#include "stm32g431xx.h"
+#include "../SPI/Spi.h"
+#include "TFT_ILI9341.h"
+
+void TFT_Init()
+{
+
+}
+
