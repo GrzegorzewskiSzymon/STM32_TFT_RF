ce1616d76ae0cd8e2caed1797be11bc84e0d0e8a
GrzegorzewskiSzymon <szymon.s.grzegorzewski@gmail.com>
2023-01-12 11:52:28 +0100

    add configuration of SPI2,  to communicate with XPT2046

---
+++ XPT2046.c
@@ -0,0 +1,25 @@
+/*
+ * XPT2046.c
+ *
+ *  Created on: Jan 11, 2023
+ *      Author: Szymon Grzegorzewski
+ */
+#include <stdint.h>
+#include "stm32g431xx.h"
+#include "XPT2046.h"
+#include "../RegistersConfig/RegistersConfig.h"
+
+//
+// SPI
+//
+
+void SPI_XPT2046_Setup()
+{
+	Spi2_Setup();
+}
+
+void Spi_XPT2046_Send(uint8_t *byte, uint32_t length)
+{
+	Spi2_Send(byte, length);
+}
+
---
+++ XPT2046.h
@@ -0,0 +1,15 @@
+/*
+ * XPT2046.h
+ *
+ *  Created on: Jan 11, 2023
+ *      Author: Szymon Grzegorzewski
+ */
+
+#ifndef LIBRARIES_TOUCHSCREEN_XPT2046_H_
+#define LIBRARIES_TOUCHSCREEN_XPT2046_H_
+
+void SPI_XPT2046_Setup();
+void Spi_XPT2046_Send(uint8_t *byte, uint32_t length);
+
+
+#endif /* LIBRARIES_TOUCHSCREEN_XPT2046_H_ */
