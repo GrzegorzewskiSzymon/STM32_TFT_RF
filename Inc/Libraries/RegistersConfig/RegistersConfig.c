5ff7494a21f46473ae5fa0370781f332617917e5
GrzegorzewskiSzymon <szymon.s.grzegorzewski@gmail.com>
2023-01-16 18:58:29 +0100

    add spi3 functions for future communication     with nrf24l01

--- Inc/Libraries/RegistersConfig/RegistersConfig.c
+++ Inc/Libraries/RegistersConfig/RegistersConfig.c
@@ -96,6 +96,84 @@ void Systick_Setup()
  				   |  (SysTick_CTRL_TICKINT_Msk);  //Exception request
 }
 
+
+//
+// SPI 2
+//
+
+void Spi3_Setup()
+{
+	RCC->APB1ENR1 |= RCC_APB1ENR1_SPI3EN;
+										//MSB first (by default)
+										//Clock polarity to 0 when idle (by default)
+										//The first clock transition is the first data capture edge (by default)
+										//Data size 8-bit (by default)
+										//Motorola frame format (by default)
+										//No NSS pulse (by default)
+	SPI3->CR1 |= SPI_CR1_MSTR;			//Master configuration
+	SPI3->CR1 |= (4<<SPI_CR1_BR_Pos);	//fPCLK/32 = 5,3125Mhz
+	SPI3->CR1 |= (1<<8) | (1<<9);  		//Software Slave Management
+	SPI3->CR2 = 0;
+	SPI3->CR2 |= SPI_CR2_FRXTH;//RXNE event is generated if the FIFO level is greater than or equal to 1/4 (8-bit)
+}
+
+void Spi3_Send(uint8_t *byte, uint32_t length)
+{
+
+    while (length > 0U)
+    {
+    	//not sure if necessary
+    	if (((SPI3->SR)&(1<<1)))//Wait for TXE bit to set -> This will indicate that the buffer is empty
+    	{
+    		*((volatile uint8_t *) &SPI3->DR) = (*byte);//Load the data into the Data Register
+    		byte++;
+    		length--;
+    	}
+
+    }
+
+    //not sure if necessary
+	//Wait for BSY bit to Reset -> This will indicate that SPI is not busy in communication
+	while (((SPI3->SR)&(1<<7))) {};
+}
+
+uint8_t SPI3_Receive_8b(uint8_t *data)
+{
+	if(SPI3->SR & SPI_SR_RXNE) // if there is data
+	{
+		*data = SPI3->DR;
+
+		return 1;
+	}
+	return 0 ;
+}
+
+void SPI3_Transreceive_8b(uint8_t *dataTx, uint16_t lengthTx, uint8_t *dataRx, uint16_t lengthRx )
+{
+    while (lengthTx > 0 || lengthRx > 0)
+    {
+    	if(lengthTx > 0)
+    	{
+    		if (((SPI3->SR)&(1<<1)))//Wait for TXE bit to set -> This will indicate that the buffer is empty
+    		{
+    			*((volatile uint8_t *) &SPI3->DR) = (*dataTx);//Load the data into the Data Register
+    			dataTx++;
+    			lengthTx--;
+    		}
+    	}
+    	if(lengthRx > 0)
+    	{
+    		if(SPI3->SR & SPI_SR_RXNE) // if there is data
+    		{
+    			*dataRx = SPI3->DR;
+    			dataRx++;
+    			lengthRx--;
+    		}
+    	}
+    }
+}
+
+
 //
 // IRQ Handlers
 //
