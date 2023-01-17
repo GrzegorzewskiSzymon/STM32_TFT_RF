37e9a13886767765e730e563c05a376df5342703
GrzegorzewskiSzymon <szymon.s.grzegorzewski@gmail.com>
2023-01-17 16:55:09 +0100

    add basic functions to communicate with NRF24L01

--- Inc/Libraries/RF/NRF24L01.c
+++ Inc/Libraries/RF/NRF24L01.c
@@ -29,7 +29,44 @@ void Spi_NRF24L01_Transreceive(uint8_t *dataTx, uint16_t lengthTx, uint8_t *data
 	Spi3_Transreceive_8b(dataTx, lengthTx, dataRx, lengthRx);
 }
 
+void NRF24L01_WriteReg(uint8_t reg, uint8_t data)
+{
+	uint8_t buf[2];
+	buf[0] = (1<<5) | reg;
+	buf[1] = data;
 
+	Spi_NRF24L01_Send(buf, 2);
+
+}
+
+void NRF24L01_WriteReg_Multi(uint8_t reg, uint8_t *data, uint16_t size)
+{
+	uint8_t buf;
+	buf = (1<<5) | reg;
+
+	Spi_NRF24L01_Send(&buf, 2);
+	Spi_NRF24L01_Send(data, size);
+
+}
+
+uint8_t NRF24L01_ReadReg(uint8_t reg)
+{
+	uint8_t dataRx;
+	Spi_NRF24L01_Transreceive(&reg, 1, &dataRx, 1);
+	return dataRx;
+
+}
+
+void NRF24L01_ReadReg_Multi(uint8_t reg, uint8_t *data, uint16_t size)
+{
+	Spi_NRF24L01_Transreceive(&reg, 1, data, size);
+}
+
+void NRF24L01_SendCommand(uint8_t cmd)
+{
+	Spi_NRF24L01_Send(&cmd, 1);
+
+}
 void NRF24L01_Setup()
 {
 
