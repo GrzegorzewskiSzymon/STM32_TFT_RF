5a592e164d3ba63f832b0c578a0bb447e4093c35
GrzegorzewskiSzymon <szymon.s.grzegorzewski@gmail.com>
2023-03-04 09:49:00 +0100

    add files for gui

---
+++ Inc/Libraries/TFT_GUI/TFT_GUI.c
@@ -0,0 +1,8 @@
+/*
+ * TFT_GUI.c
+ *
+ *  Created on: 4 Mar 2023
+ *      Author: Szymon Grzegorzewski
+ */
+
+
---
+++ Inc/Libraries/TFT_GUI/TFT_GUI.h
@@ -0,0 +1,8 @@
+/*
+ * TFT_GUI.h
+ *
+ *  Created on: 4 Mar 2023
+ *      Author: Szymon Grzegorzewski
+ */
+
+
